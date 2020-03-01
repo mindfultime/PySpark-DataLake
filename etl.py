97ca1bb518ff613c8481d0affe1d68e63cbc537e
akshu <akshobhya2015@outlook.com>
2020-03-01 17:22:28 +0545

    Enhanced code for deployment in
    Signed-off-by: akshu <akshobhya2015@outlook.com>

--- etl.py
+++ etl.py
@@ -1,10 +1,14 @@
 # pip install pyspark
 
+
+# import findspark
 import configparser
 import os
 from dataProcessing import process_song, process_log, check_df, write_to_s3
 from star_schema import dim_songs, dim_users, dim_time, dim_artists, fact_songplays
 from pyspark.sql import SparkSession
+
+# findspark.init()
 
 # setting configparser for aws and filepaths to instantiate immediately
 # config obj for parsing aws credential
@@ -21,8 +25,8 @@ os.environ["AWS_SECRET_ACCESS_KEY"] = config['default']['AWS_SECRET_ACCESS_KEY']
 URL = config['S3']['DEND-URL']
 S3 = config['S3']['S3-SPARK']
 
-song_cfg = config['SONG']['SONG_FILE']
-log_cfg = config['LOG']['LOG_FILE']
+song_cfg = config['SONG']['SONG_FILES']
+log_cfg = config['LOG']['LOG_FILES']
 
 
 def main():
@@ -33,6 +37,9 @@ def main():
     # starting spark session
     spark = SparkSession \
         .builder \
+        .config("master", "yarn") \
+        .config("spark.driver.memory", "10g") \
+        .config("spark.driver.memory", "8g") \
         .config('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:2.7.3') \
         .appName('Sparkify') \
         .getOrCreate()
@@ -60,21 +67,21 @@ if __name__ == '__main__':
     DF = {'song': process_song(spark, song_filepath),
           'log': process_log(spark, log_filepath)}
 
+    # creating dim_artists based on song
+    dim_artists = dim_artists(DF['song'])
+
+    # check_df(dim_artists, "dim_artists")
+
+    # writing dim_artists to s3
+    write_to_s3(dim_artists, {'S3': S3, 'table': "dim_artists"})
+
     # creating dim_users based on log
     dim_users = dim_users(DF['log'])
 
     # check_df(dim_users, "dim_users")
 
     # writing dim_users to s3 partitioned by userId and level
-    write_to_s3(dim_users, {'S3': S3, 'table': 'dim_users'}, ("userId", "level"))
-
-    # creating dim_artists based on song
-    dim_artists = dim_artists(DF['song'])
-
-    # check_df(dim_artists, "dim_artists")
-
-    # writing dim_artists to s3 partitioned by song_id and artist_id
-    write_to_s3(dim_artists, {'S3': S3, 'table': "dim_artists"}, ("artist_id"))
+    write_to_s3(dim_users, {'S3': S3, 'table': 'dim_users'}, ("user_id", "level"))
 
     # creating dim_songs dataframe based on song
     dim_songs = dim_songs(DF['song'])
@@ -98,6 +105,6 @@ if __name__ == '__main__':
     # check_df(fact_songplays(DF), "fact_songplays")
 
     # writing fact_songplays to s3 partitioned by year and moth
-    write_to_s3(fact_songplays, {'S3': S3, 'table': "fact_songplays"}, ("year", "month"))
+    write_to_s3(fact_songplays, {'S3': S3, 'table': "fact_songplays"}, ("artist_id", "song_id"))
 
     spark.stop()
